61bb9f7f18459b90397f948fdaa17f1dbe596d25
Nika Kopadze <nikolozkopadze@gmail.com>
2024-01-02 21:26:41 +0400

    Refactor SiteFooter component to include footer links

--- components/layouts/SiteFooter.tsx
+++ components/layouts/SiteFooter.tsx
@@ -1,9 +1,52 @@
-import * as React from 'react';
+import * as React from "react";
+import { Shell } from "./Shell";
+import Link from "next/link";
 
 interface SiteFooterProps {}
 
+const footerLinks = [
+  {
+    title: "changelog",
+    href: "/changelog",
+  },
+  {
+    title: "faq",
+    href: "/faq",
+  },
+  {
+    title: "Privacy",
+    href: "#",
+  },
+  {
+    title: "Terms",
+    href: "#",
+  },
+  {
+    title: "Contact",
+    href: "#",
+  },
+];
+
 const SiteFooter: React.FC<SiteFooterProps> = ({}) => {
-  return <div>SiteFooter</div>;
+  return (
+    <Shell
+      as="footer"
+      className="flex flex-row justify-between mx-auto  max-w-5xl px-0 py-10 uppercase"
+    >
+      <p className="text-black/50">SPOT-SYNC 2024© All Rights Reserved</p>
+      <nav>
+        <ul className="flex space-x-4">
+          {footerLinks.map(link => (
+            <li key={link.title}>
+              <Link href={link.href} className="text-black/50">
+                {link.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </Shell>
+  );
 };
 
 export default SiteFooter;
